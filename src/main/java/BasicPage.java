05b61cf1634a87d2f4a334db6366bc59e168eb22
Ksenija Beocanin <ksenijabeocanin@gmail.com>
2024-11-01 11:41:46 +0100

    Created LoginPage by extending BasicPage

---
+++ src/main/java/BasicPage.java
@@ -0,0 +1,12 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public abstract class BasicPage {
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+
+    public BasicPage(WebDriver driver, WebDriverWait wait) {
+        this.driver = driver;
+        this.wait = wait;
+    }
+}
