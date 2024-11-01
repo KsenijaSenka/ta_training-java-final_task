a5c3df588e8b56f478a9332f9737cd4e63de75db
Ksenija Beocanin <ksenijabeocanin@gmail.com>
2024-11-01 11:48:25 +0100

    Created BasicTest class

---
+++ src/test/java/BasicTest.java
@@ -0,0 +1,31 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public abstract class BasicTest {
+    protected String url = "https://www.saucedemo.com/";
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+    protected LoginPage loginPage;
+
+    protected void setUpChrome() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    protected void setUpFirefox() {
+        WebDriverManager.firefoxdriver().setup();
+        driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+    }
+
+    protected void setUpEdge() {
+        WebDriverManager.edgedriver().setup();
+        driver = new EdgeDriver();
+        driver.manage().window().maximize();
+    }
+}
