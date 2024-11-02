24c05df7fd2d99da0b33544ebe78893f9c513227
Ksenija Beocanin <ksenijabeocanin@gmail.com>
2024-11-02 12:02:36 +0100

    verified unsuccessful login with empty credentials in Chrome

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,4 +1,11 @@
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class LoginTest extends BasicTest {
 
@@ -38,4 +45,33 @@ public class LoginTest extends BasicTest {
 //        loginPage.enterPassword(password);
 //        loginPage.clickOnLoginButton();
 //    }
+@Test
+public void verifyUnsuccessfulLoginWithEmptyCredentials() {
+    String username = "random_user";
+    String password = "invalid";
+
+    setUpChrome();
+    driver.navigate().to(url);
+    wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    loginPage = new LoginPage(driver, wait);
+    loginPage.enterUsername(username);
+    loginPage.enterPassword(password);
+    loginPage.clearUsernameInput(loginPage.getUsernameInput());
+    loginPage.clearPasswordInput(loginPage.getPasswordInput());
+
+    loginPage.clickOnLoginButton();
+
+    wait
+            .withMessage("Error message for missing username should be present")
+            .until(ExpectedConditions.textToBePresentInElement(
+                    loginPage.getErrorMessageElement(), "Username is required"));
+
+
+
+//    assertTrue(
+//            loginPage.getErrorMessageElement().contains("Username is required"),
+//            "Error message for missing username should be present"
+//    );
+
+}
 }
